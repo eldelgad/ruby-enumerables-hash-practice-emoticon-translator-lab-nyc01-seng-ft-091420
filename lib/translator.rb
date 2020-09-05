1c1b28f9adc0700343334e9a5e8bf3a6a799b82f
Elias Delgado <evdelgado99@gmail.com>
2020-09-05 20:40:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,11 +26,11 @@ def get_japanese_emoticon(emote_file, emojis)
     
   end
   
-  if emojis == !library[emojis][:japanese]
+  if emojis = !library[emojis][:japanese]
     # binding.pry
     library[emojis][:japanese]
   else
-    p "Sorry, that emoticon was not found"
+    
   end
   
   # emojis ? library[emojis][:japanese] : "Sorry, that emoticon was not found"
